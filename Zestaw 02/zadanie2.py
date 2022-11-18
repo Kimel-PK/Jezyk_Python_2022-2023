3845110af799d042343dcdab4a79ce954e17430b
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2022-11-18 13:16:39 +0100

    Zadanie2 - mała poprawka

--- Zestaw 02/zadanie2.py
+++ Zestaw 02/zadanie2.py
@@ -3,5 +3,5 @@ import time
 
 while (1) :
 	now = datetime.now()
-	print ('►      ' + str(now.hour).zfill(1) + ':' + str(now.minute).zfill(1) + ':' + str(now.second).zfill(2) + '      ◄', end='\r')
+	print ('►      ' + str(now.hour).zfill(2) + ':' + str(now.minute).zfill(2) + ':' + str(now.second).zfill(2) + '      ◄', end='\r')
 	time.sleep(0.5)
